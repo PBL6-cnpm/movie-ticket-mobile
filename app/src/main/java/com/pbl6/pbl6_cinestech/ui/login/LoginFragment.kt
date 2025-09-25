2451c6b5dd2b4a2ad64073a552306026eaea4966
Hoang <duongminhhoang333@gmail.com>
2025-09-25 22:10:38 +0700

    feat: login screen

---
+++ app/src/main/java/com/pbl6/pbl6_cinestech/ui/login/LoginFragment.kt
@@ -0,0 +1,16 @@
+package com.pbl6.pbl6_cinestech.ui.login
+
+import com.pbl6.pbl6_cinestech.databinding.FragmentLoginBinding
+import hoang.dqm.codebase.base.activity.BaseFragment
+
+
+class LoginFragment : BaseFragment<FragmentLoginBinding, LoginViewModel>() {
+    override fun initView() {
+    }
+
+    override fun initListener() {
+    }
+
+    override fun initData() {
+    }
+}
