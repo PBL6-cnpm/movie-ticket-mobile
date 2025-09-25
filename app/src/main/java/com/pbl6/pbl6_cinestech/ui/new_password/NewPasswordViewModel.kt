13acd4fa4e2b8213c02e5b884eb990eb3742a5f5
Hoang <duongminhhoang333@gmail.com>
2025-09-25 22:09:57 +0700

    feat: forgot screen

---
+++ app/src/main/java/com/pbl6/pbl6_cinestech/ui/new_password/NewPasswordViewModel.kt
@@ -0,0 +1,4 @@
+package com.pbl6.pbl6_cinestech.ui.new_password
+
+class NewPasswordViewModel {
+}
---
+++ app/src/main/java/com/pbl6/pbl6_cinestech/ui/password_changed/PasswordChangedViewModel.kt
@@ -0,0 +1,4 @@
+package com.pbl6.pbl6_cinestech.ui.password_changed
+
+class PasswordChangedViewModel {
+}
