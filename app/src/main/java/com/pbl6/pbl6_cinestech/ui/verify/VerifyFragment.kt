b44ba0936c622d1da6fccb916c0f14ffb4036b44
Hoang <duongminhhoang333@gmail.com>
2025-09-25 22:11:29 +0700

    verify

---
+++ app/src/main/java/com/pbl6/pbl6_cinestech/ui/verify/VerifyFragment.kt
@@ -0,0 +1,17 @@
+package com.pbl6.pbl6_cinestech.ui.verify
+
+import com.pbl6.pbl6_cinestech.databinding.FragmentVerifyBinding
+import hoang.dqm.codebase.base.activity.BaseFragment
+
+
+class VerifyFragment : BaseFragment<FragmentVerifyBinding, VerifyViewModel>() {
+    override fun initView() {
+
+    }
+
+    override fun initListener() {
+    }
+
+    override fun initData() {
+    }
+}
