d2aba416390524d166eeb293044600c0dbb93551
Hoang <duongminhhoang333@gmail.com>
2025-09-25 22:11:12 +0700

    feat: register screen

---
+++ app/src/main/java/com/pbl6/pbl6_cinestech/ui/register/RegisterFragment.kt
@@ -0,0 +1,19 @@
+package com.pbl6.pbl6_cinestech.ui.register
+
+import com.pbl6.pbl6_cinestech.databinding.FragmentRegisterBinding
+import hoang.dqm.codebase.base.activity.BaseFragment
+
+
+class RegisterFragment : BaseFragment<FragmentRegisterBinding, RegisterViewModel>() {
+    override fun initView() {
+        TODO("Not yet implemented")
+    }
+
+    override fun initListener() {
+        TODO("Not yet implemented")
+    }
+
+    override fun initData() {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/pbl6/pbl6_cinestech/ui/register/RegisterViewModel.kt
@@ -0,0 +1,6 @@
+package com.pbl6.pbl6_cinestech.ui.register
+
+import hoang.dqm.codebase.base.viewmodel.BaseViewModel
+
+class RegisterViewModel : BaseViewModel() {
+}
