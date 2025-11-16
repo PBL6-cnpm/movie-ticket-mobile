5436cec6c9c3457b8f36a8bc0af0fd04b1bfae39
Hoang <duongminhhoang333@gmail.com>
2025-11-16 15:32:10 +0700

    feat: login screen ui

--- app/src/main/java/com/pbl6/pbl6_cinestech/ui/login/LoginFragment.kt
+++ app/src/main/java/com/pbl6/pbl6_cinestech/ui/login/LoginFragment.kt
@@ -1,17 +1,25 @@
 package com.pbl6.pbl6_cinestech.ui.login
 
+import android.text.InputType
 import android.util.Log
+import android.view.MotionEvent
+import android.view.View
 import android.widget.Toast
+import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModelProvider
 import com.pbl6.pbl6_cinestech.R
 import com.pbl6.pbl6_cinestech.data.repository.RepositoryProvider
 import com.pbl6.pbl6_cinestech.databinding.FragmentLoginBinding
 import com.pbl6.pbl6_cinestech.ui.home.HomeViewModel
+import com.pbl6.pbl6_cinestech.ui.main.MainViewModel
 import com.pbl6.pbl6_cinestech.utils.AppConstants
 import com.pbl6.pbl6_cinestech.utils.SecurePrefs
 import hoang.dqm.codebase.base.activity.BaseFragment
 import hoang.dqm.codebase.base.activity.navigate
+import hoang.dqm.codebase.base.activity.onBackPressed
+import hoang.dqm.codebase.base.activity.popBackStack
 import hoang.dqm.codebase.utils.singleClick
+import kotlin.getValue
 
 
 class LoginFragment : BaseFragment<FragmentLoginBinding, LoginViewModel>() {
@@ -19,6 +27,7 @@ class LoginFragment : BaseFragment<FragmentLoginBinding, LoginViewModel>() {
         get() = LoginViewModel.LoginViewModelFactory(
             RepositoryProvider.authRepository
         )
+    private val mainViewModel: MainViewModel by activityViewModels()
 
     override fun initView() {
         setUpObserver()
@@ -29,14 +38,13 @@ class LoginFragment : BaseFragment<FragmentLoginBinding, LoginViewModel>() {
             Log.d("check login", "setUpObserver: $value")
             if (value?.success == true){
                 if(value.data == null) return@observe
-                Log.d("check login", "setUpObserver: ${value.data.accessToken}")
                 if (value.data.account.status == AppConstants.PENDING){
                     Toast.makeText(requireContext(), value.data.message, Toast.LENGTH_SHORT).show()
                 }else if (value.data.account.status == AppConstants.ACTIVE){
                     // navigate to main
                     SecurePrefs.saveTokens(requireContext(), value.data.accessToken, value.data.refreshToken)
+                    mainViewModel.setLogin(true)
                     navigate(R.id.homeFragment)
-
                 }
             }else{
                 value?.message?.let {
@@ -50,13 +58,49 @@ class LoginFragment : BaseFragment<FragmentLoginBinding, LoginViewModel>() {
         binding.btnLogin.singleClick {
             val email = binding.edtEmail.text.toString()
             val password = binding.edtPassword.text.toString()
-            Log.d("check login", "setUpObserver: login")
+            if (email.isEmpty()){
+                Toast.makeText(requireContext(), "Email is empty", Toast.LENGTH_SHORT).show()
+                return@singleClick
+            } else if (password.isEmpty()){
+                Toast.makeText(requireContext(), "Password is empty", Toast.LENGTH_SHORT).show()
+                return@singleClick
+            }
             viewModel.login(email, password)
         }
         binding.tvForgotPassword.singleClick {
             navigate(R.id.forgotPasswordFragment)
         }
+        binding.tvRegister.singleClick {
+            navigate(R.id.registerFragment)
+        }
+
+        binding.imgToggleVisiblePassword.singleClick {
+            togglePasswordVisibility()
+        }
+        onBackPressed {
+            popBackStack(R.id.homeFragment)
+        }
     }
+
+    private var isPasswordVisible = false
+
+    private fun togglePasswordVisibility() {
+        val inputType = if (isPasswordVisible) {
+            InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+        } else {
+            InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+        }
+        binding.edtPassword.inputType = inputType
+
+        val iconRes = if (isPasswordVisible) R.drawable.icon_invisible else R.drawable.icon_visible
+        binding.imgToggleVisiblePassword.setImageResource(iconRes)
+
+        binding.edtPassword.setSelection(binding.edtPassword.text.length)
+        isPasswordVisible = !isPasswordVisible
+    }
+
+
+
 
     override fun initData() {
     }
--- app/src/main/java/com/pbl6/pbl6_cinestech/ui/register/RegisterFragment.kt
+++ app/src/main/java/com/pbl6/pbl6_cinestech/ui/register/RegisterFragment.kt
@@ -1,17 +1,36 @@
 package com.pbl6.pbl6_cinestech.ui.register
 
+import android.os.Build
+import android.os.Handler
+import android.os.Looper
+import android.text.InputType
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
+import androidx.annotation.RequiresExtension
 import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.snackbar.Snackbar
 import com.pbl6.pbl6_cinestech.R
+import com.pbl6.pbl6_cinestech.data.repository.RepositoryProvider
 import com.pbl6.pbl6_cinestech.databinding.FragmentRegisterBinding
+import com.pbl6.pbl6_cinestech.ui.main.MainViewModel
 import hoang.dqm.codebase.base.activity.BaseFragment
 import hoang.dqm.codebase.base.activity.navigate
 import hoang.dqm.codebase.utils.singleClick
 
 
 class RegisterFragment : BaseFragment<FragmentRegisterBinding, RegisterViewModel>() {
+    private val mainViewModel by activityViewModels<MainViewModel>()
+
+    override val viewModelFactory: ViewModelProvider.Factory?
+        get() = RegisterViewModel.RegisterViewModelFactory(
+            RepositoryProvider.authRepository
+        )
     override fun initView() {
         setUpPasswordConfirmation()
-
+        setUpObserver()
     }
 
     fun setUpPasswordConfirmation() {
@@ -25,10 +44,11 @@ class RegisterFragment : BaseFragment<FragmentRegisterBinding, RegisterViewModel
         }
     }
 
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
     override fun initListener() {
         binding.btnRegister.singleClick {
             val email = binding.edtEmail.text.toString()
-            val password = binding.edtFullname.text.toString()
+            val password = binding.edtPassword.text.toString()
             val fullName = binding.edtFullname.text.toString()
             viewModel.register(email, password, fullName)
         }
@@ -38,8 +58,76 @@ class RegisterFragment : BaseFragment<FragmentRegisterBinding, RegisterViewModel
         binding.tvLogin.singleClick {
             navigate(R.id.loginFragment)
         }
+        binding.imgToggleVisiblePassword.singleClick {
+            togglePasswordVisibility()
+        }
+
+        binding.imgToggleConfirmVisiblePassword.singleClick {
+            toggleConfirmPasswordVisibility()
+        }
     }
 
+    private var isConfirmPasswordVisible = false
+
+    private fun toggleConfirmPasswordVisibility() {
+        val inputType = if (isConfirmPasswordVisible) {
+            InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+        } else {
+            InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+        }
+        binding.edtConfirmPassword.inputType = inputType
+
+        val iconRes = if (isConfirmPasswordVisible) R.drawable.icon_invisible else R.drawable.icon_visible
+        binding.imgToggleConfirmVisiblePassword.setImageResource(iconRes)
+
+        binding.edtConfirmPassword.setSelection(binding.edtConfirmPassword.text.length)
+        isConfirmPasswordVisible = !isConfirmPasswordVisible
+    }
+
+
+    private var isPasswordVisible = false
+
+    private fun togglePasswordVisibility() {
+        val inputType = if (isPasswordVisible) {
+            InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+        } else {
+            InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+        }
+        binding.edtPassword.inputType = inputType
+
+        val iconRes = if (isPasswordVisible) R.drawable.icon_invisible else R.drawable.icon_visible
+        binding.imgToggleVisiblePassword.setImageResource(iconRes)
+
+        binding.edtPassword.setSelection(binding.edtPassword.text.length)
+        isPasswordVisible = !isPasswordVisible
+    }
+
+
     override fun initData() {
+    }
+
+    fun setUpObserver() {
+        viewModel.registerResultLiveData.observe(viewLifecycleOwner) { value ->
+            Log.d("check register", "setUpObserver: $value")
+            if (value?.success == true) {
+                if (value.data == null) return@observe
+                Toast.makeText(requireContext(), "Register success", Toast.LENGTH_SHORT).show()
+                navigate(R.id.loginFragment, isPop = true)
+            } else if (value?.success == false) {
+                if (value.message.length > 16) {
+                    val snackbar =
+                        Snackbar.make(binding.root, value.message, 1500)
+                            .setAction("OK") {
+                            }
+                    val snackbarView = snackbar.view
+                    val textView = snackbarView.findViewById<TextView>(com.google.android.material.R.id.snackbar_text)
+                    textView.maxLines = 5
+                    snackbar.setAction("OK") {
+                    }
+                    snackbar.show()
+                } else
+                    Toast.makeText(requireContext(), value.message, Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
