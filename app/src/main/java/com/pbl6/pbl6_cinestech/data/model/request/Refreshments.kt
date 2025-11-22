fe8543d17e018755d7f65d037ca816841d8e29d2
Hoang <duongminhhoang333@gmail.com>
2025-11-22 11:38:03 +0700

    feat: fix refreshments

--- app/src/main/java/com/pbl6/pbl6_cinestech/data/model/request/Refreshments.kt
+++ app/src/main/java/com/pbl6/pbl6_cinestech/data/model/request/Refreshments.kt
@@ -1,7 +1,12 @@
 package com.pbl6.pbl6_cinestech.data.model.request
 
+import com.google.gson.annotations.SerializedName
+
 class Refreshments(
     var refreshmentId: String,
-    var quantity: Int
+    var quantity: Int,
+    var price:Int = 0,
+    var name: String = "",
+    var imgPath: String = ""
 ) {
 }
