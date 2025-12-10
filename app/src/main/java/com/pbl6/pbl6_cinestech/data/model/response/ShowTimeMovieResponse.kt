603b74323fdec6671a04c78340feff764b69be36
Hoang <duongminhhoang333@gmail.com>
2025-12-10 23:50:02 +0700

    feat: update ui show time

---
+++ app/src/main/java/com/pbl6/pbl6_cinestech/data/model/response/ShowTimeMovieResponse.kt
@@ -0,0 +1,9 @@
+package com.pbl6.pbl6_cinestech.data.model.response
+
+class ShowTimeMovieResponse(
+    var id: String,
+    var timeStart: String,
+    var showDate: String,
+    var movie: MovieResponse
+) {
+}
--- app/src/main/java/com/pbl6/pbl6_cinestech/data/model/response/Time.kt
+++ app/src/main/java/com/pbl6/pbl6_cinestech/data/model/response/Time.kt
@@ -2,6 +2,11 @@ package com.pbl6.pbl6_cinestech.data.model.response
 
 class Time(
     var id: String,
-    var time: String
+    var time: String,
+    var totalSeats: Int,
+    var availableSeats: Int,
+    var occupiedSeats: Int,
+    var roomId: String,
+    var roomName: String
 ) {
 }
